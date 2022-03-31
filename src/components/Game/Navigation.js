5fe12baf88f021c7e3516291ac3e03c7c7c118f2
Noah Reece <noah.w.reece@gmail.com>
2022-03-31 13:20:15 -0400

    navigation orientation; passage reuse limit

--- src/components/Game/Navigation.js
+++ src/components/Game/Navigation.js
@@ -1,44 +1,85 @@
 import OptionBox from "./OptionBox"
 
 function Navigation({curGameInfo, map, updateCurRoom, setEndType}) {
-    const {curLocation, stringPath} = curGameInfo
+    const {curLocation, stringPath, entryDirection} = curGameInfo
     const curRoom = map.find(room => room.path === curLocation[0]);
-    const curRoomNavOptions= [
-        {
-            choiceText : "go left",
-            flavorText : curRoom.westPassageType
-        },
-        {
-            choiceText: "go right",
-            flavorText: curRoom.eastPassageType
-        },
-        {
-            choiceText: "go back to last room",
-            flavorText: curRoom.southPassageType + "🧶"
+
+    // get passages and directions
+    const westNav = curRoom.westPassageType
+        ? {
+            flavorText: curRoom.westPassageType,
+            path: curRoom.path + '0'
         }
-    ]
+        : {}
+    const eastNav = curRoom.eastPassageType
+    ? {
+        flavorText: curRoom.eastPassageType,
+        path: curRoom.path + '1'
+    }
+    : {}
+    const southNav = curRoom.southPassageType
+    ? {
+        flavorText: curRoom.southPassageType + '🧶',
+        path: curRoom.path.slice(0,-1)
+    }
+    : {}
+
+    // // create nav options
+    let leftNav = { choiceText : "go left" };
+    let rightNav = { choiceText: "go right" };
+    let backNav = { choiceText: "go back to last room" };
+
+    // assign passage types to room options based on entryDirection
+    switch (entryDirection) {
+        case 'south':
+            leftNav = {...leftNav, ...westNav}
+            rightNav = {...rightNav, ...eastNav}
+            backNav = {...backNav, ...southNav}
+            break;
+        
+        case 'east':
+            leftNav = {...leftNav, ...southNav}
+            rightNav = {...rightNav, ...westNav}
+            backNav = {...backNav, ...eastNav}
+            break;
+        
+        case 'west':
+            leftNav = {...leftNav, ...eastNav}
+            rightNav = {...rightNav, ...southNav}
+            backNav = {...backNav, ...westNav}
+        default:
+            break;
+    }
+
+    // set room options
+    const curRoomNavOptions = [];
+    if (leftNav.flavorText) {
+        curRoomNavOptions.push(leftNav)
+    }
+    if (rightNav.flavorText) {
+        curRoomNavOptions.push(rightNav)
+    }
+    curRoomNavOptions.push(backNav);
 
     function updateRoomByPath(newRoomPath){
-        
-        const newRoom = map.find(room => {
-            return room.path === newRoomPath
-        })
-        updateCurRoom(newRoom)
+        if (!newRoomPath) {
+            alert('trying to leave labyrinth!')
+        } else {
+            const newRoom = map.find(room => {
+                return room.path === newRoomPath
+            })
+            updateCurRoom(newRoom)
+        }
     }
-    //so now, we need to update the current room to the room
-    //with the consolelogged path
 
     function handleClick(event){
-        
         switch (event.target.innerText) {
-            case "go left": updateRoomByPath(curRoom.path + "0");
+            case "go left": updateRoomByPath(leftNav.path);
                 break;
-            case "go right": updateRoomByPath(curRoom.path + "1");
+            case "go right": updateRoomByPath(rightNav.path);
                 break;
             case "go back to last room": 
-                if (curRoom.path === "0"){updateRoomByPath("0")}
-                else {updateRoomByPath(curLocation[1])}
-                // else {updateRoomByPath(curRoom.path.slice(0, curRoom.path.length - 1))}
+                updateRoomByPath(backNav.path)
                 break;
         }
     }
@@ -48,7 +89,10 @@ function Navigation({curGameInfo, map, updateCurRoom, setEndType}) {
         roomFlavor = 'You found Theseus!'
         setEndType('win')
     }
-    else {roomFlavor = `You find yourself in room ${curLocation[0]}`}
+    // else {roomFlavor = `You find yourself in room ${curLocation[0]}`}
+    else {roomFlavor = curRoomNavOptions.length > 1 ? `How do you proceed?` : 'You have reached a dead end'}
+
+
     return (
         <div className="navigation">
             <h2>{roomFlavor}</h2>
--- src/components/Game/OptionBox.js
+++ src/components/Game/OptionBox.js
@@ -2,27 +2,17 @@ import Option from "./Option"
 import { v4 as uuid } from "uuid";
 
 function OptionBox({options, handleClick}) {
-    let renderCount = 0;
     const renderedOptions = options.map(option => {
-        if (option.flavorText){
-            ++ renderCount
         return (
             <div key={uuid()} className="option">
                 <h4> {option.flavorText} </h4>
                 <Option handleClick={handleClick} choice = {option.choiceText} />
             </div>
-        )}
-        else {return null}
+        )
     })
-
-    let roomFlavor = ''
-
-    if (renderCount === 1) {roomFlavor = 'You have reached a dead end'}
-    else {roomFlavor = `You see ${renderCount} ways to leave the room:`}
 
     return (
         <div className="optionBox">
-            <h3>{roomFlavor}</h3>
             {renderedOptions}
         </div>
     )
